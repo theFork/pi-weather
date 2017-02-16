aca84902abfbd032dde4a89821edd3fb95782bd6
Simon Gansen <simon@sineband.de>
2017-02-16 18:01:34 +0000

    Sense brightness and write to database

--- piweather-logger.py
+++ piweather-logger.py
@@ -3,6 +3,7 @@ import sqlite3
 import time
 
 from drivers.htu21df import HTU21DF as htu21df
+from drivers.tsl256x import tsl256x as tsl256x
 
 from piweather import PiWeather
 
@@ -16,15 +17,19 @@ def main():
     db_cursor.execute('''CREATE TABLE IF NOT EXISTS weather
                          (timestamp INTEGER, temperature INTEGER, humidity INTEGER, brightness INTEGER)''')
 
-    # Fetch measured values
+    # Init sensors
     htu21df.htu_reset()
+    tsl2560 = tsl256x.TSL256x()
+
+    # Fetch measured values
     temperature = int(htu21df.read_temperature()*100)
     humidity = int(htu21df.read_humidity()*100)
+    brightness = tsl2560.lux()
     timestamp = int(time.time())
 
     # Insert values
-    db_cursor.execute('''INSERT INTO weather (timestamp, temperature, humidity)
-                       VALUES (?, ?, ?)''', (timestamp, temperature, humidity))
+    db_cursor.execute('''INSERT INTO weather (timestamp, temperature, humidity, brightness)
+                       VALUES (?, ?, ?, ?)''', (timestamp, temperature, humidity, brightness))
 
     # Save and close
     db_connection.commit()
