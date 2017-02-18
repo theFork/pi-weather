1e4d168b4780dafd4f2c68f262ae3993d0b681b2
Simon Gansen <simon@sineband.de>
2017-02-18 19:50:21 +0000

    Perform calculations on data directly with SQLite

--- database_reader.py
+++ database_reader.py
@@ -3,6 +3,7 @@ import sqlite3
 import time
 
 class DatabaseReader:
+    QUERY = "SELECT timestamp, temperature/100.0, humidity/100.0, brightness FROM weather"
 
     def __init__(self, filename):
         self.filename = filename
@@ -19,19 +20,18 @@ class DatabaseReader:
 
         # Read all data if hours is zero
         if hours == 0:
-            db_cur.execute("SELECT timestamp, temperature, humidity, brightness FROM weather")
+            db_cur.execute(self.QUERY)
         else:
             current_timestamp = int(time.time())
             select_timestamp = current_timestamp - (hours * 3600)
             print("Selecting from timestamp " + str(select_timestamp))
-            db_cur.execute("SELECT timestamp, temperature, humidity, brightness FROM weather WHERE timestamp>=?",
-                            (select_timestamp, ))
+            db_cur.execute(self.QUERY + "WHERE timestamp>=?", (select_timestamp, ))
 
         # TODO: This appears quiet inefficient
         for row in db_cur.fetchall():
             self.timestamps.append(datetime.datetime.fromtimestamp(row[0]))
-            self.temperature_values.append(row[1] / 100.0)
-            self.humidity_values.append(row[2] / 100.0)
+            self.temperature_values.append(row[1])
+            self.humidity_values.append(row[2])
             self.brightness_values.append(row[3])
 
         # Close database
