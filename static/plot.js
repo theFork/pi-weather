c6d673d19e9c386dec4892a6f523e0d3b227be80
Simon Gansen <simon@sineband.de>
2017-02-18 23:55:20 +0000

    Plot luminosity

--- static/plot.js
+++ static/plot.js
@@ -1,8 +1,7 @@
 function plot_data() {
     $.getJSON("/get_data", function(data) {
         var grid = {
-            gridLineWidth: 2,
-            gridLineColor: 'rgb(235,235,235)',
+            gridLineColor: 'rgb(200,200,200)',
             drawGridlines: true
         };
         temperature_plot = []
@@ -13,14 +12,58 @@ function plot_data() {
         for (var i=0; i<data.timestamp.length; ++i) {
             humidity_plot.push([new Date(data.timestamp[i]), data.humidity[i]]);
         }
-//        alert(temperature_plot.join('\n'));
-        $.jqplot ('chart', [temperature_plot, humidity_plot], {
+        brightness_plot = []
+        for (var i=0; i<data.timestamp.length; ++i) {
+            brightness_plot.push([new Date(data.timestamp[i]), data.brightness[i]]);
+        }
+
+        $.jqplot ('chart', [brightness_plot, temperature_plot, humidity_plot], {
             axes: {
                 xaxis: {
-                    renderer:$.jqplot.DateAxisRenderer
+                    renderer: $.jqplot.DateAxisRenderer,
+                    tickOptions: {
+                        angle: 45,
+                    }
+                },
+                yaxis: {
+                    min:  0,
+                    max: 40,
+                    tickInterval: 2,
+                    label: 'Temperature [°C]',
+                },
+                y2axis: {
+                    min: 30,
+                    max: 80,
+                    tickInterval: 5,
+                    label: 'Humidity [%]',
                 }
             },
-            grid: grid
+            grid: grid,
+            series: [
+                { // Luminosity
+                    color: 'rgba(127, 127, 127, 0.3)',
+                    showMarker: false,
+                    shadow: false,
+                    fill:true,
+
+                },
+                { // Temperature
+                    yaxis: 'yaxis',
+                    color: 'rgba(255, 0, 0, 0.6)',
+                    showMarker: false,
+                    rendererOptions: {
+                        smooth: true
+                    },
+                },
+                { // Humidity
+                    yaxis: 'y2axis',
+                    color: 'rgba(0, 0, 255, 0.6)',
+                    showMarker: false,
+                    rendererOptions: {
+                        smooth: true
+                    },
+                },
+            ]
         });
     });
 }
